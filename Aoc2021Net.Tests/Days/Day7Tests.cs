40a57e8dca64305461ca7f2d2eb613b155b345eb
Maxim K. Dobroselsky <m.hall.muz@gmail.com>
2021-12-07 16:19:27 +0300

    Day 7 solved

--- Aoc2021Net.Tests/Days/Day7Tests.cs
+++ Aoc2021Net.Tests/Days/Day7Tests.cs
@@ -4,11 +4,11 @@ using NUnit.Framework;
 namespace Aoc2021Net.Tests.Days
 {
     [DayDataPart1(
-        @"", 0)]
-    [DayDataPart1(0)]
+        @"16,1,2,0,4,2,7,1,2,14", 37)]
+    [DayDataPart1(353800)]
     [DayDataPart2(
-        @"", 0)]
-    [DayDataPart2(0)]
+        @"16,1,2,0,4,2,7,1,2,14", 168)]
+    [DayDataPart2(98119739)]
     [TestFixture]
     public sealed class Day7Tests : DayTests<Day7Tests>
     {
--- Aoc2021Net/Utilities/RangeUtilities.cs
+++ Aoc2021Net/Utilities/RangeUtilities.cs
@@ -1,9 +1,11 @@
-﻿using System;
-
-namespace Aoc2021Net.Utilities
+﻿namespace Aoc2021Net.Utilities
 {
     internal static class RangeUtilities
     {
-        public static bool ContainsValue(this Range range, int value) => value >= range.Start.Value && value <= range.End.Value;
+        public static bool ContainsValue(this Range range, int value) =>
+            value >= range.Start.Value && value <= range.End.Value;
+
+        public static IEnumerable<int> Enumerate(this Range range) =>
+            Enumerable.Range(range.Start.Value, range.End.Value - range.Start.Value + 1);
     }
 }
