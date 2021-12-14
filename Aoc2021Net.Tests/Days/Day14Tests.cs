3e81e087dc00599a8b0860ac4f746c017755c7f0
Maxim K. Dobroselsky <m.hall.muz@gmail.com>
2021-12-14 17:15:30 +0300

    Day 14 solved

--- Aoc2021Net.Tests/Days/Day14Tests.cs
+++ Aoc2021Net.Tests/Days/Day14Tests.cs
@@ -4,11 +4,45 @@ using NUnit.Framework;
 namespace Aoc2021Net.Tests.Days
 {
     [DayDataPart1(
-        @"", 0)]
-    [DayDataPart1(0)]
+        @"NNCB
+
+          CH -> B
+          HH -> N
+          CB -> H
+          NH -> C
+          HB -> C
+          HC -> B
+          HN -> C
+          NN -> C
+          BH -> H
+          NC -> B
+          NB -> B
+          BN -> B
+          BB -> N
+          BC -> B
+          CC -> N
+          CN -> C", 1588)]
+    [DayDataPart1(3048)]
     [DayDataPart2(
-        @"", 0)]
-    [DayDataPart2(0)]
+        @"NNCB
+
+          CH -> B
+          HH -> N
+          CB -> H
+          NH -> C
+          HB -> C
+          HC -> B
+          HN -> C
+          NN -> C
+          BH -> H
+          NC -> B
+          NB -> B
+          BN -> B
+          BB -> N
+          BC -> B
+          CC -> N
+          CN -> C", 2188189693529)]
+    [DayDataPart2(3288891573057)]
     [TestFixture]
     public sealed class Day14Tests : DayTests<Day14Tests>
     {
