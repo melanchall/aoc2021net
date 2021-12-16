ea7e57a966408fedfea0b1ff85a12a3258031106
Maxim K. Dobroselsky <m.hall.muz@gmail.com>
2021-12-16 16:22:08 +0300

    Day 16 solved

--- Aoc2021Net.Tests/Days/Day16Tests.cs
+++ Aoc2021Net.Tests/Days/Day16Tests.cs
@@ -3,12 +3,22 @@ using NUnit.Framework;
 
 namespace Aoc2021Net.Tests.Days
 {
-    [DayDataPart1(
-        @"", 0)]
-    [DayDataPart1(0)]
-    [DayDataPart2(
-        @"", 0)]
-    [DayDataPart2(0)]
+    [DayDataPart1(@"D2FE28", 6)]
+    [DayDataPart1(@"8A004A801A8002F478", 16)]
+    [DayDataPart1(@"620080001611562C8802118E34", 12)]
+    [DayDataPart1(@"C0015000016115A2E0802F182340", 23)]
+    [DayDataPart1(@"A0016C880162017C3686B18A3D4780", 31)]
+    [DayDataPart1(929)]
+    [DayDataPart2(@"D2FE28", 2021)]
+    [DayDataPart2(@"C200B40A82", 3)]
+    [DayDataPart2(@"04005AC33890", 54)]
+    [DayDataPart2(@"880086C3E88112", 7)]
+    [DayDataPart2(@"CE00C43D881120", 9)]
+    [DayDataPart2(@"D8005AC2A8F0", 1)]
+    [DayDataPart2(@"F600BC2D8F", 0)]
+    [DayDataPart2(@"9C005AC2F8F0", 0)]
+    [DayDataPart2(@"9C0141080250320F1802104A08", 1)]
+    [DayDataPart2(911945136934)]
     [TestFixture]
     public sealed class Day16Tests : DayTests<Day16Tests>
     {
