4d619b502209371720c7ad9f3dfbfacea2850609
Maxim K. Dobroselsky <m.hall.muz@gmail.com>
2021-12-17 11:40:52 +0300

    Day 17 solved

--- Aoc2021Net.Tests/Days/Day17Tests.cs
+++ Aoc2021Net.Tests/Days/Day17Tests.cs
@@ -3,12 +3,10 @@ using NUnit.Framework;
 
 namespace Aoc2021Net.Tests.Days
 {
-    [DayDataPart1(
-        @"", 0)]
-    [DayDataPart1(0)]
-    [DayDataPart2(
-        @"", 0)]
-    [DayDataPart2(0)]
+    [DayDataPart1(@"target area: x=20..30, y=-10..-5", 45)]
+    [DayDataPart1(8646)]
+    [DayDataPart2(@"target area: x=20..30, y=-10..-5", 112)]
+    [DayDataPart2(5945)]
     [TestFixture]
     public sealed class Day17Tests : DayTests<Day17Tests>
     {
